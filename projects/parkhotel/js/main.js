8e6351e5a47ecc46ee1e2bbe26a5e74bdd316805
Nadezhda Kovaleva <Nadia.kv13@gmail.com>
2021-02-10 14:25:34 +0300

    Add corrections

--- projects/parkhotel/js/main.js
+++ projects/parkhotel/js/main.js
@@ -1618,7 +1618,75 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _import_modules__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./import/modules */ "./src/js/import/modules.js");
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+
+
+function createRipple(event) {
+  console.log("click");
+  var button = event.currentTarget;
+  var circle = document.createElement("span");
+  var diameter = Math.max(button.clientWidth, button.clientHeight);
+  var radius = diameter / 2;
+  circle.style.width = circle.style.height = "".concat(diameter, "px");
+  circle.style.left = "".concat(event.clientX - button.getBoundingClientRect().left - radius, "px");
+  circle.style.top = "".concat(event.clientY - button.getBoundingClientRect().top - radius, "px");
+  circle.classList.add("ripple");
+  var ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+var buttons = document.getElementsByClassName("btn");
+
+var _iterator = _createForOfIteratorHelper(buttons),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var button = _step.value;
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      console.log(event.target);
+      setTimeout(function () {
+        console.log(event.target.href);
+        window.location = event.target.href ? event.target.href : event.target.parentElement.href;
+      }, 600);
+    });
+    button.addEventListener("click", createRipple);
+  }
+  /*(function() {
+    const buttons = document.querySelectorAll(".btn");
+  
+    buttons.forEach(button => {
+      ["mouseenter", "mouseout"].forEach(evt => {
+        button.addEventListener(evt, e => {
+          let parentOffset = button.getBoundingClientRect(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+  
+          const span = button.getElementsByTagName("span");
+  
+          span[0].style.top = relY + "px";
+          span[0].style.left = relX + "px";
+        });
+      });
+    });
+  })();*/
+
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
 
 /***/ })
 
